4d632e56174458b7b06930cca0ddf23337da6cb9
roy gerbiel <governmentvan@outlook.com>
2019-12-06 17:07:03 -0700

    fixed ttt and added choose

--- src/main/java/io/banditoz/mchelper/commands/TTTCommand.java
+++ src/main/java/io/banditoz/mchelper/commands/TTTCommand.java
@@ -28,7 +28,9 @@ public class TTTCommand extends Command {
     @Override
     protected void onCommand(CommandEvent ce) {
         MessageReceivedEvent e = ce.getEvent();
-        int howMany = random.nextInt(4);
+        int howMany = 0;
+        if (ce.getCommandArgs()[1].matches("\\d+"))
+            howMany = Integer.parseInt(ce.getCommandArgs()[1]);
         if (howMany == 0)
             howMany++;
         if (ce.getCommandArgsString().contains(",")) {
@@ -41,32 +43,26 @@ public class TTTCommand extends Command {
             if (players.contains("bandit") || players.contains("Hayden") || players.contains("hayden"))
             {
                 sendPrivateMessage(e.getJDA().getUserById("163094867910590464"),"TRAITOR!!");
-                ce.sendReply("<@163094867910590464>");
             }
             if (players.contains("josh") || players.contains("Superskish") || players.contains("superskish"))
             {
                 sendPrivateMessage(e.getJDA().getUserById("375040836829839360"),"TRAITOR!!");
-                ce.sendReply("<@375040836829839360>");
             }
             if (players.contains("Kadin") || players.contains("kanislupus") || players.contains("Kanislupus") || players.contains("kadin"))
             {
                 sendPrivateMessage(e.getJDA().getUserById("384834373012160523"),"TRAITOR!!");
-                ce.sendReply("<@384834373012160523>");
             }
             if (players.contains("ashton") || players.contains("Ashton") || players.contains("xradusa") || players.contains("Xradiusa"))
             {
                 sendPrivateMessage(e.getJDA().getUserById("226880247692394496"),"TRAITOR!!");
-                ce.sendReply("<@226880247692394496>");
             }
             if (players.contains("mccord") || players.contains("McCord") || players.contains("xradusm") || players.contains("Xradiusm"))
             {
                 sendPrivateMessage(e.getJDA().getUserById("339820351376850944"),"TRAITOR!!");
-                ce.sendReply("<@339820351376850944>");
             }
             if (players.contains("kyler") || players.contains("Kyler") || players.contains("EvelerKyvans") || players.contains("Eveler Kyvans"))
             {
                 sendPrivateMessage(e.getJDA().getUserById("404837963697225729"),"TRAITOR!!");
-                ce.sendReply("<@404837963697225729>");
             }
         } else
             ce.sendReply("No valid users found");
