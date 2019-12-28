4eba7e4c6c1a4c24b82f3a02ca98f081a3a8dd7d
HeyBanditoz <banditoz@protonmail.com>
2019-12-27 19:06:11 -0700

    Bot owners can do stuff now

--- src/main/java/io/banditoz/mchelper/commands/DefaultChannelCommand.java
+++ src/main/java/io/banditoz/mchelper/commands/DefaultChannelCommand.java
@@ -6,6 +6,8 @@ import io.banditoz.mchelper.utils.Help;
 import io.banditoz.mchelper.utils.database.Database;
 import net.dv8tion.jda.api.utils.MiscUtil;
 
+import static io.banditoz.mchelper.commands.logic.CommandPermissions.*;
+
 public class DefaultChannelCommand extends Command {
     @Override
     public String commandName() {
@@ -23,7 +25,7 @@ public class DefaultChannelCommand extends Command {
         if (ce.getCommandArgs().length == 1) {
             ce.sendReply(Database.getInstance().getGuildDataById(ce.getGuild()).getDefaultChannel() + " is the default channel.");
         }
-        else if (ce.getEvent().getGuild().getOwner().getId().equals(ce.getEvent().getAuthor().getId())) {
+        else if (isBotOwner(ce.getEvent().getAuthor()) || isGuildOwner(ce.getEvent().getAuthor(), ce.getGuild())) {
             MiscUtil.parseSnowflake(ce.getCommandArgs()[1]); // check if it is a valid discord snowflake (I think)
             Database.getInstance().getGuildDataById(ce.getGuild()).setDefaultChannel(ce.getCommandArgs()[1]);
             Database.getInstance().saveDatabase();
--- src/main/java/io/banditoz/mchelper/commands/logic/CommandPermissions.java
+++ src/main/java/io/banditoz/mchelper/commands/logic/CommandPermissions.java
@@ -2,12 +2,13 @@ package io.banditoz.mchelper.commands.logic;
 
 import io.banditoz.mchelper.utils.Settings;
 import io.banditoz.mchelper.utils.SettingsManager;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
 
 import java.util.List;
 
 public class CommandPermissions {
-    public static boolean isBotOwner(User attempter){
+    public static boolean isBotOwner(User attempter) {
         Settings settings = SettingsManager.getInstance().getSettings();
         List<String> botOwners = settings.getBotOwners();
         for (String owner : botOwners) {
@@ -16,5 +17,9 @@ public class CommandPermissions {
             }
         }
         return false;
+    }
+
+    public static boolean isGuildOwner(User attempter, Guild g) {
+        return g.getOwnerIdLong() == attempter.getIdLong();
     }
 }
