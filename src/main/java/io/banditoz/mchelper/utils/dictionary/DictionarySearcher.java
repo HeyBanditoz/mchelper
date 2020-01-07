bf0a392e3c0f8d3ebbc974c3b5b11a023fb2db1a
HeyBanditoz <banditoz@protonmail.com>
2020-01-07 15:02:56 -0700

    Cache dictionary results

--- src/main/java/io/banditoz/mchelper/utils/dictionary/DictionarySearcher.java
+++ src/main/java/io/banditoz/mchelper/utils/dictionary/DictionarySearcher.java
@@ -4,18 +4,31 @@ import io.banditoz.mchelper.MCHelper;
 import io.banditoz.mchelper.utils.HttpResponseException;
 import io.banditoz.mchelper.utils.SettingsManager;
 import okhttp3.Request;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class DictionarySearcher {
+    private static DictionaryCache cache = new DictionaryCache();
+    private static Logger logger = LoggerFactory.getLogger(DictionarySearcher.class);
+
     public static DictionaryResult search(String word) throws IOException, HttpResponseException {
-        Request request = new Request.Builder()
-                .url("https://owlbot.info/api/v3/dictionary/" + word)
-                .addHeader("Authorization", "Token " + SettingsManager.getInstance().getSettings().getOwlBotToken())
-                .build();
-        String json = MCHelper.performHttpRequest(request);
-        LoggerFactory.getLogger(DictionarySearcher.class).debug(json);
-        return MCHelper.getObjectMapper().readValue(json, DictionaryResult.class);
+        // first, search the cache
+        if (cache.getDefinition(word) != null) {
+            logger.debug("Cache hit for " + word);
+            return cache.getDefinition(word);
+        }
+        else {
+            Request request = new Request.Builder()
+                    .url("https://owlbot.info/api/v3/dictionary/" + word)
+                    .addHeader("Authorization", "Token " + SettingsManager.getInstance().getSettings().getOwlBotToken())
+                    .build();
+            String json = MCHelper.performHttpRequest(request);
+            logger.debug(json);
+            DictionaryResult definition = MCHelper.getObjectMapper().readValue(json, DictionaryResult.class);
+            cache.putDefinition(definition);
+            return definition;
+        }
     }
 }
