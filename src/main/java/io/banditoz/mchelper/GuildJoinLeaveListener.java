e7dcff875783753a8b790faaed00c3e97ee842ef
HeyBanditoz <banditoz@protonmail.com>
2020-02-18 23:32:22 -0700

    report id in guild leave/joins

--- src/main/java/io/banditoz/mchelper/GuildJoinLeaveListener.java
+++ src/main/java/io/banditoz/mchelper/GuildJoinLeaveListener.java
@@ -3,6 +3,7 @@ package io.banditoz.mchelper;
 import io.banditoz.mchelper.commands.logic.CommandUtils;
 import io.banditoz.mchelper.utils.database.Database;
 import io.banditoz.mchelper.utils.database.GuildData;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -14,7 +15,7 @@ public class GuildJoinLeaveListener extends ListenerAdapter {
     public void onGuildMemberJoin(@Nonnull GuildMemberJoinEvent event) {
         GuildData gd = Database.getInstance().getGuildDataNull(event.getGuild());
         if (!(gd == null)) {
-            CommandUtils.sendReply(event.getMember().getUser().getName() + " has joined the guild.", event.getGuild().getTextChannelById(gd.getDefaultChannel()));
+            CommandUtils.sendReply(getNameAndDiscriminator(event.getUser()) + " has joined the guild.", event.getGuild().getTextChannelById(gd.getDefaultChannel()));
         }
     }
 
@@ -22,7 +23,11 @@ public class GuildJoinLeaveListener extends ListenerAdapter {
     public void onGuildMemberLeave(@Nonnull GuildMemberLeaveEvent event) {
         GuildData gd = Database.getInstance().getGuildDataNull(event.getGuild());
         if (!(gd == null)) {
-            CommandUtils.sendReply(event.getMember().getUser().getName() + " has left the guild.", event.getGuild().getTextChannelById(gd.getDefaultChannel()));
+            CommandUtils.sendReply(getNameAndDiscriminator(event.getUser()) + " has left the guild.", event.getGuild().getTextChannelById(gd.getDefaultChannel()));
         }
+    }
+
+    private String getNameAndDiscriminator(User u) {
+        return u.getName() + "#" + u.getDiscriminator() + " (" + u.getId() + ")";
     }
 }
