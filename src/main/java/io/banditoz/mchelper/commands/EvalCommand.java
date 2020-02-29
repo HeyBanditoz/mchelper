54c2d79b37f8f9389f30c350da500270c213a188
HeyBanditoz <banditoz@protonmail.com>
2020-02-29 16:19:44 -0700

    Switch scripting engine to Groovy
    Yeah yeah the javascript one is deprecated

--- src/main/java/io/banditoz/mchelper/commands/EvalCommand.java
+++ src/main/java/io/banditoz/mchelper/commands/EvalCommand.java
@@ -12,14 +12,14 @@ import javax.script.ScriptEngineManager;
 public class EvalCommand extends ElevatedCommand {
     private ScriptEngine engine;
     public EvalCommand() {
-        engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine = new ScriptEngineManager().getEngineByName("groovy");
 
     }
 
     @Override
     public Help getHelp() {
-        return new Help(commandName(), true).withParameters("\\`\\`\\`js<newline>\\`\\`\\`")
-                .withDescription("Evaluates JavaScript. If you don't use code blocks, a return is added to the beginning of the code," +
+        return new Help(commandName(), true).withParameters("\\`\\`\\`groovy<newline>\\`\\`\\`")
+                .withDescription("Evaluates Groovy. If you don't use code blocks, a return is added to the beginning of the code," +
                         "otherwise, if you are using code blocks, you should return something.");
     }
 
@@ -31,27 +31,16 @@ public class EvalCommand extends ElevatedCommand {
     // Partially stolen from https://github.com/DV8FromTheWorld/Yui/blob/0eaeed13d97ab40225542a40014f79566e430daf/src/main/java/net/dv8tion/discord/commands/EvalCommand.java
     @Override
     protected void onCommand(CommandEvent ce) {
-        boolean blocked = false;
         String args;
-        if (ce.getCommandArgsString().startsWith("```js")) {
-            args = ce.getCommandArgsString().replace("```js", "").replace("```", "");
-            blocked = true;
+        if (ce.getCommandArgsString().startsWith("```groovy")) {
+            args = ce.getCommandArgsString().replace("```groovy", "").replace("```", "");
         }
         else {
             args = ce.getCommandArgsString();
         }
         try {
-            engine.eval("var imports = new JavaImporter(" +
-                    "java.io," +
-                    "java.lang," +
-                    "java.util," +
-                    "Packages.net.dv8tion.jda.api," +
-                    "Packages.net.dv8tion.jda.api.entities," +
-                    "Packages.net.dv8tion.jda.api.entities.impl," +
-                    "Packages.net.dv8tion.jda.api.managers," +
-                    "Packages.net.dv8tion.jda.api.managers.impl," +
-                    "Packages.net.dv8tion.jda.api.utils);");
-            engine.put("e", ce.getEvent());
+            String imports = "import net.dv8tion.jda.*;\n";
+            engine.put("ce", ce);
             engine.put("args", ce.getCommandArgs());
             engine.put("jda", ce.getEvent().getJDA());
             if (ce.getEvent().isFromType(ChannelType.TEXT))
@@ -59,11 +48,7 @@ public class EvalCommand extends ElevatedCommand {
                 engine.put("guild", ce.getEvent().getGuild());
                 engine.put("member", ce.getEvent().getMember());
             }
-            Object out = engine.eval("(function() {" +
-                                            "with (imports) {" + (blocked ? "" : "return ") +
-                                            (args) +
-                                            "}" +
-                                            "})();");
+            Object out = engine.eval(imports + args);
             if (out == null) {
                 ce.sendReply(null); // checked in CommandUtils
             }
