da0b7c469becea61b29a569a83d724d6e5c0e8f7
HeyBanditoz <banditoz@protonmail.com>
2020-04-03 03:01:21 -0600

    Make the guild leave/join listener an embedded message
    Mostly so people can't puppet the bot into sending false messages. More will follow for important messages like these if there are any, I cannot think of any, it's three in the morning.

--- src/main/java/io/banditoz/mchelper/GuildJoinLeaveListener.java
+++ src/main/java/io/banditoz/mchelper/GuildJoinLeaveListener.java
@@ -1,21 +1,34 @@
 package io.banditoz.mchelper;
 
-import io.banditoz.mchelper.commands.logic.CommandUtils;
 import io.banditoz.mchelper.utils.database.Database;
 import io.banditoz.mchelper.utils.database.GuildData;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.awt.Color;
+import java.time.Instant;
 
 public class GuildJoinLeaveListener extends ListenerAdapter {
+    private static String DEFAULT_AVATAR = "https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png";
+
     @Override
     public void onGuildMemberJoin(@Nonnull GuildMemberJoinEvent event) {
         GuildData gd = Database.getInstance().getGuildDataNull(event.getGuild());
         if (!(gd == null)) {
-            CommandUtils.sendReply(getNameAndDiscriminator(event.getUser()) + " has joined the guild.", event.getGuild().getTextChannelById(gd.getDefaultChannel()));
+            MessageEmbed me = new EmbedBuilder()
+                    .setTitle((event.getUser().isBot() ? "Bot" : "User") + " joined the guild.")
+                    .setThumbnail(event.getUser().getAvatarUrl() == null ? DEFAULT_AVATAR : event.getUser().getAvatarUrl())
+                    .setColor(Color.GREEN)
+                    .setDescription(getNameAndDiscriminator(event.getUser()))
+                    .setFooter(event.getUser().getId())
+                    .setTimestamp(Instant.now())
+                    .build();
+            event.getGuild().getTextChannelById(gd.getDefaultChannel()).sendMessage(me).queue();
         }
     }
 
@@ -23,11 +36,19 @@ public class GuildJoinLeaveListener extends ListenerAdapter {
     public void onGuildMemberRemove(@Nonnull GuildMemberRemoveEvent event) {
         GuildData gd = Database.getInstance().getGuildDataNull(event.getGuild());
         if (!(gd == null)) {
-            CommandUtils.sendReply(getNameAndDiscriminator(event.getUser()) + " has left the guild.", event.getGuild().getTextChannelById(gd.getDefaultChannel()));
+            MessageEmbed me = new EmbedBuilder()
+                    .setTitle((event.getUser().isBot() ? "Bot" : "User") + " left the guild.")
+                    .setThumbnail(event.getUser().getAvatarUrl() == null ? DEFAULT_AVATAR : event.getUser().getAvatarUrl())
+                    .setColor(Color.RED)
+                    .setDescription(getNameAndDiscriminator(event.getUser()))
+                    .setFooter(event.getUser().getId())
+                    .setTimestamp(Instant.now())
+                    .build();
+            event.getGuild().getTextChannelById(gd.getDefaultChannel()).sendMessage(me).queue();
         }
     }
 
     private String getNameAndDiscriminator(User u) {
-        return u.getName() + "#" + u.getDiscriminator() + " (" + u.getId() + ")";
+        return u.getName() + "#" + u.getDiscriminator();
     }
 }
