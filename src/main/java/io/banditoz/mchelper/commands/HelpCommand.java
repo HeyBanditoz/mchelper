65e9bb1df79a7f65bc1162b4be7411a7cbceb9a0
HeyBanditoz <banditoz@protonmail.com>
2020-04-17 22:50:37 -0600

    Update HelpCommand

--- src/main/java/io/banditoz/mchelper/commands/HelpCommand.java
+++ src/main/java/io/banditoz/mchelper/commands/HelpCommand.java
@@ -6,12 +6,10 @@ import io.banditoz.mchelper.commands.logic.CommandEvent;
 import io.banditoz.mchelper.utils.Help;
 import io.banditoz.mchelper.utils.database.dao.GuildConfigDaoImpl;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class HelpCommand extends Command {
-    private List<Command> commands = new ArrayList<>();
+    private final TreeMap<String, Help> helps = new TreeMap<>(); // TreeMap keeps it sorted
 
     @Override
     public String commandName() {
@@ -19,14 +17,14 @@ public class HelpCommand extends Command {
     }
 
     public HelpCommand() {
-        commands.add(this); // let's add ourselves into the mix, eh?
+        helps.put(this.commandName(), getHelp()); // let's add ourselves into the mix, eh?
         for (Object registeredListener : MCHelper.getJDA().getRegisteredListeners()) {
             if (registeredListener instanceof Command) { // don't add the regex listeners
-                commands.add((Command) registeredListener);
+                Command c = (Command) registeredListener;
+                helps.put(c.commandName(), c.getHelp());
             }
         }
-        commands.sort(Comparator.comparing(Command::commandName)); // sort alphabetically by command name
-        LOGGER.info(commands.size() + " commands registered.");
+        LOGGER.info(helps.size() + " commands registered.");
     }
 
     @Override
@@ -37,11 +35,21 @@ public class HelpCommand extends Command {
 
     @Override
     protected void onCommand(CommandEvent ce) {
-        StringBuilder sb = new StringBuilder("Current list of commands (prefix with `")
-                .append(new GuildConfigDaoImpl().getConfig(ce.getGuild()).getPrefix()).append("`):\n");
-        for (Command c : commands) {
-            sb.append(c.getHelp()).append("\n"); // TODO split this into multiple messages in ce.sendReply somehow, this could grow too large in the future!
+        if (!(ce.getCommandArgs().length > 1)) {
+            char prefix = new GuildConfigDaoImpl().getConfig(ce.getGuild()).getPrefix();
+            StringBuilder sb = new StringBuilder();
+            for (String s : helps.keySet()) {
+                sb.append('`').append(prefix).append(s).append("` ");
+            }
+            ce.sendReply(sb.toString());
         }
-        ce.sendReply(sb.toString());
+        else {
+            if (helps.containsKey(ce.getCommandArgs()[1])) {
+                ce.sendReply(helps.get(ce.getCommandArgs()[1]).toString());
+            }
+            else {
+                ce.sendReply("That command does not exist.");
+            }
+        }
     }
 }
