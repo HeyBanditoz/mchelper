3eb9c835c21f60c347eda90cf9fcdc876b988664
HeyBanditoz <banditoz@protonmail.com>
2020-12-18 16:54:44 -0700

    Patch up RussianRouletteCommand a bit
    Should solve all NullPointerExceptions.

--- src/main/java/io/banditoz/mchelper/commands/RussianRouletteCommand.java
+++ src/main/java/io/banditoz/mchelper/commands/RussianRouletteCommand.java
@@ -31,23 +31,37 @@ public class RussianRouletteCommand extends Command {
     @Override
     protected Status onCommand(CommandEvent ce) throws Exception {
         GuildVoiceState vs = ce.getEvent().getMember().getVoiceState();
-        if (!ce.getEvent().getGuild().getSelfMember().getPermissions().contains(Permission.VOICE_MOVE_OTHERS)) {
-            ce.sendReply("I do not have VOICE_MOVE_OTHERS!");
-            return Status.FAIL;
-        }
-        if (vs == null) {
-            ce.sendReply("You are not in a voice channel.");
-            return Status.FAIL;
+        if (!check(ce, vs)) {
+            return Status.FAIL; // bot does not have permissions or not in a voice channel
         }
         ce.sendReply(ce.getEvent().getMember().getEffectiveName() + " spins the cylinder...");
         ce.getMCHelper().getSES().schedule(() -> {
             try {
-                Member toKick = vs.getChannel().getMembers().get(ThreadLocalRandom.current().nextInt(vs.getChannel().getMembers().size()));
-                vs.getGuild().kickVoiceMember(toKick).queue(unused -> ce.sendReply("**BANG!**"));
+                if (check(ce, vs)) {
+                    Member toKick = vs.getChannel().getMembers().get(ThreadLocalRandom.current().nextInt(vs.getChannel().getMembers().size()));
+                    vs.getGuild().kickVoiceMember(toKick).queue(unused -> ce.sendReply("**BANG!**"));
+                }
             } catch (Exception ex) {
                 CommandUtils.sendExceptionMessage(ce.getEvent(), ex, LOGGER);
             }
         }, 3, TimeUnit.SECONDS);
         return Status.SUCCESS;
+    }
+
+    /**
+     * Checks preconditions for this command.
+     *
+     * @return true if the bot has the permission or the executor is in a voice channel, false if not.
+     */
+    private boolean check(CommandEvent ce, GuildVoiceState vs) {
+        if (!ce.getEvent().getGuild().getSelfMember().getPermissions().contains(Permission.VOICE_MOVE_OTHERS)) {
+            ce.sendReply("I do not have VOICE_MOVE_OTHERS!");
+            return false;
+        }
+        if (vs.getChannel() == null) {
+            ce.sendReply("You are not in a voice channel.");
+            return false;
+        }
+        return true;
     }
 }
