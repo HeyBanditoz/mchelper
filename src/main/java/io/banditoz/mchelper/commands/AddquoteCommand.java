91dcb6c108261c03be4c4dc12b3e1930300744d1
HeyBanditoz <banditoz@protonmail.com>
2022-07-08 17:21:14 -0600

    AddquoteCommand changes
    * Now uses a button instead of a reaction to edit quote
    * Probably buggy and uses wayyy too much state, but fuck it :)

--- src/main/java/io/banditoz/mchelper/commands/AddquoteCommand.java
+++ src/main/java/io/banditoz/mchelper/commands/AddquoteCommand.java
@@ -1,9 +1,10 @@
 package io.banditoz.mchelper.commands;
 
-
 import io.banditoz.mchelper.commands.logic.Command;
 import io.banditoz.mchelper.commands.logic.CommandEvent;
 import io.banditoz.mchelper.commands.logic.Requires;
+import io.banditoz.mchelper.interactions.ButtonInteractable;
+import io.banditoz.mchelper.interactions.WrappedButtonClickEvent;
 import io.banditoz.mchelper.stats.Status;
 import io.banditoz.mchelper.utils.EventHandler;
 import io.banditoz.mchelper.utils.Help;
@@ -12,37 +13,36 @@ import io.banditoz.mchelper.utils.database.dao.QuotesDao;
 import io.banditoz.mchelper.utils.database.dao.QuotesDaoImpl;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.cache2k.Cache;
+import org.cache2k.Cache2kBuilder;
 
 import java.sql.SQLException;
-import java.util.concurrent.Future;
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 import java.util.regex.Pattern;
 
 @Requires(database = true)
 public class AddquoteCommand extends Command {
+    private QuotesDao qd;
+    // hacky, surely there is a better way to do this instead of a silly cache in the same class?
+    private final Cache<Long, CommandEventAndQuote> cache = new Cache2kBuilder<Long, CommandEventAndQuote>() {}
+            .expireAfterWrite(60, TimeUnit.SECONDS)
+            .suppressExceptions(false)
+            .build();
 
-    private final EventHandler<MessageReactionAddEvent> handler = new EventHandler<>() {
-        @Override
-        public void eventConsumer(GenericEvent event) {
-            MessageReactionAddEvent messageReactionAddEvent = (MessageReactionAddEvent)event;
-            if (messageReactionAddEvent.getUser().isBot() || events.get(messageReactionAddEvent.getUserId()) == null) {
-                return;
-            }
-            if (events.containsKey(messageReactionAddEvent.getUserId())) {
-                events.get(messageReactionAddEvent.getUserId()).accept(messageReactionAddEvent);
-            }
-        }
-    };
     private final EventHandler<MessageReceivedEvent> listener = new EventHandler<>() {
-        private final Pattern QUOTE_PARSER = Pattern.compile("^[\"“”](.*?)[\"“”]\\s+");
+        private static final Pattern QUOTE_PARSER = Pattern.compile("^[\"“”](.*?)[\"“”]\\s+");
 
         @Override
         public void eventConsumer(GenericEvent event) {
-            MessageReceivedEvent messageReceivedEvent = (MessageReceivedEvent)event;
+            MessageReceivedEvent messageReceivedEvent = (MessageReceivedEvent) event;
             if (!events.containsKey(messageReceivedEvent.getAuthor().getId())) {
                 return;
             }
@@ -52,8 +52,6 @@ public class AddquoteCommand extends Command {
             events.get(messageReceivedEvent.getAuthor().getId()).accept(messageReceivedEvent);
         }
     };
-
-    private static final int timeoutSecs = 60;
 
     @Override
     public String commandName() {
@@ -68,78 +66,91 @@ public class AddquoteCommand extends Command {
 
     @Override
     protected Status onCommand(CommandEvent ce) throws Exception {
-        if (!ce.getMCHelper().getJDA().getRegisteredListeners().contains(handler)) {
-            ce.getMCHelper().getJDA().addEventListener(handler);
+        // hacky, fix this (maybe with DI?)
+        if (qd == null) {
+            qd = new QuotesDaoImpl(ce.getDatabase());
         }
         if (!ce.getMCHelper().getJDA().getRegisteredListeners().contains(listener)) {
             ce.getMCHelper().getJDA().addEventListener(listener);
         }
+
         QuotesDao qd = new QuotesDaoImpl(ce.getDatabase());
-        int id = 0;
-        Message message = null;
-        try {
+        int id;
+        MessageBuilder message;
+        Button b = Button.primary(UUID.randomUUID().toString(), "✏️");
+
+        if (Character.isDigit(ce.getCommandArgs()[1].charAt(0))) {
             long messageId = Long.parseLong(ce.getCommandArgs()[1]);
-            if (String.valueOf(messageId).length() != 18) { // TODO Optimize this check
-                throw new NumberFormatException();
+            if (ce.getCommandArgs()[1].length() != 18) {
+                throw new NumberFormatException("Not the proper current snowflake length of 18!");
             }
             NamedQuote nq = NamedQuote.parseMessageId(messageId, ce.getEvent().getTextChannel());
             nq.setAuthorId(ce.getEvent().getAuthor().getIdLong());
             id = qd.saveQuote(nq);
-            message = new MessageBuilder("Quote (from message ID) added.").build();
-        } catch (NumberFormatException ex) { // it isn't an ID, let's continue
+            message = new MessageBuilder("Quote (from message ID) added.");
+        }
+        else {
             NamedQuote nq = NamedQuote.parseString(ce.getCommandArgsString());
             nq.setGuildId(ce.getGuild().getIdLong());
             nq.setAuthorId(ce.getEvent().getAuthor().getIdLong());
             id = qd.saveQuote(nq);
-            message = new MessageBuilder("Quote added.").build();
+            message = new MessageBuilder("Quote added.");
         }
-        ce.getEvent().getChannel().sendMessage(message).queue();
-        Message finalMessage = ce.getEvent().getMessage();
-        finalMessage.addReaction("✏").queue();
+        if (!cache.containsKey(ce.getEvent().getAuthor().getIdLong())) {
+            message = message.setActionRows(ActionRow.of(b));
+        }
         int finalId = id;
-        handler.addEvent(ce.getEvent().getMember().getId(), new Consumer<MessageReactionAddEvent>() {
-            private Future<?> timeout;
-            private final Consumer<Void> success = s -> handler.removeEvent(finalMessage.getAuthor().getId());
-
-            {
-                timeout = finalMessage.clearReactions().queueAfter(timeoutSecs, TimeUnit.SECONDS, success, (Consumer<Throwable>) null);
-            }
-
-            @Override
-            public void accept(MessageReactionAddEvent event) {
-                if (!finalMessage.getId().equals(event.getMessageId())) {
-                    return;
-                }
-                if (!event.getReactionEmote().getEmoji().equals("✏")) {
-                    return;
-                }
-                if (!finalMessage.getAuthor().equals(event.getUser())) {
-                    return;
-                }
-                finalMessage.clearReactions().queue();
-                timeout.cancel(false);
-                ce.sendReply("Enter your new quote (\"<new_quote>\" <author>)");
-                handler.removeEvent(finalMessage.getAuthor().getId());
-                listener.addEvent(ce.getEvent().getMember().getId(), guildMessageReceivedEvent -> {
-                    try {
-                        NamedQuote namedQuote = NamedQuote.parseString(guildMessageReceivedEvent.getMessage().getContentDisplay());
-                        qd.editQuote(finalId,namedQuote);
-                        ce.sendReply("Quote edited!");
-                    } catch (IllegalArgumentException ex) {
-                        return;
-                    } catch (SQLException ex) {
-                        ce.sendExceptionMessage(ex);
-                    }
-                    listener.removeEvent(guildMessageReceivedEvent.getAuthor().getId());
-                });
-                ce.getMCHelper().getSES().schedule(() -> {
-                    if (listener.containsEvent(ce.getEvent().getAuthor().getId())) {
-                        listener.removeEvent(ce.getEvent().getAuthor().getId());
-                        ce.sendReply(finalMessage.getAuthor().getAsMention() + " your quote edit has expired.");
-                    }
-                },5, TimeUnit.MINUTES);
+        ce.getEvent().getChannel().sendMessage(message.build()).queue(sentMessage -> {
+            User u = ce.getEvent().getAuthor();
+            if (!cache.containsKey(u.getIdLong())) {
+                cache.put(u.getIdLong(), new CommandEventAndQuote(ce, finalId));
+                ButtonInteractable bi = new ButtonInteractable(Map.of(b, this::editQuote),
+                        user -> ce.getEvent().getMessage().getAuthor().equals(user),
+                        60,
+                        sentMessage);
+                ce.getMCHelper().getButtonListener().addInteractable(bi);
             }
         });
         return Status.SUCCESS;
     }
+
+    private void editQuote(WrappedButtonClickEvent event) {
+        Message finalMessage = event.getMessage();
+        event.getEvent().editMessage(finalMessage).setActionRows(Collections.emptyList()).queue();
+        CommandEventAndQuote ceq = cache.get(event.getEvent().getUser().getIdLong());
+        if (ceq == null) {
+            event.removeListenerAndDestroy();
+            return;
+        }
+        CommandEvent ce = ceq.ce();
+        int finalId = ceq.id();
+        LOGGER.info("{} is editing quote {}.", ceq.ce.getEvent().getAuthor(), finalId);
+
+        ce.sendReply("Enter your new quote (\"<new_quote>\" <author>)");
+        listener.addEvent(event.getEvent().getMember().getId(), messageEvent -> {
+            try {
+                NamedQuote namedQuote = NamedQuote.parseString(messageEvent.getMessage().getContentDisplay());
+                qd.editQuote(finalId, namedQuote);
+                ce.sendReply("Quote edited!");
+                LOGGER.info("{} has edited quote {}.", ceq.ce.getEvent().getAuthor(), finalId);
+            } catch (IllegalArgumentException ex) {
+                return;
+            } catch (SQLException ex) {
+                ce.sendExceptionMessage(ex);
+            }
+            cache.remove(messageEvent.getAuthor().getIdLong());
+            listener.removeEvent(messageEvent.getAuthor().getId());
+            event.removeListener();
+        });
+        event.getMCHelper().getSES().schedule(() -> {
+            if (listener.containsEvent(ce.getEvent().getAuthor().getId())) {
+                cache.remove(finalMessage.getIdLong());
+                listener.removeEvent(ce.getEvent().getAuthor().getId());
+                ce.sendReply(finalMessage.getAuthor().getAsMention() + ", your quote edit has expired.");
+            }
+        },1, TimeUnit.MINUTES);
+    }
+
+    /** The poor man's tuple. */
+    private record CommandEventAndQuote(CommandEvent ce, int id) {}
 }
