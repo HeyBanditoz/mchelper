015c428328865cc41cbb90a8cbac24409352a8ea
HeyBanditoz <banditoz@protonmail.com>
2023-10-31 01:00:11 -0600

    Kludgy hotfix for 20 channels from KSL news from somehow being returned
    Still need to find the root cause but this'll patch it for now (probably in the RSS library's Channel::equals?)

--- src/main/java/io/banditoz/mchelper/motd/NewsHeadlineMotdSectionGenerator.java
+++ src/main/java/io/banditoz/mchelper/motd/NewsHeadlineMotdSectionGenerator.java
@@ -13,6 +13,7 @@ import java.awt.Color;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static io.banditoz.mchelper.utils.StringUtils.truncate;
 
@@ -28,7 +29,13 @@ public class NewsHeadlineMotdSectionGenerator extends MotdSectionGenerator {
 
         Map<Channel, List<Item>> stories = mcHelper.getRssScraper().getRssForAll(urls);
         EmbedBuilder eb = new EmbedBuilder().setTitle("News Stories Today").setColor(new Color(23, 183, 204));
-        stories.keySet().stream().sorted(Comparator.comparing(Channel::getTitle)).forEach(channel -> {
+        stories.keySet()
+                .stream()
+                .map(ChannelWrapper::new)
+                .distinct()
+                .map(ChannelWrapper::channel)
+                .sorted(Comparator.comparing(Channel::getTitle))
+                .forEach(channel -> {
             Item item = stories.get(channel).get(0);
             String title = truncate(channel.getTitle(), 253, false);
             String mdUrl = "[To Article](%s)".formatted(item.getLink().orElseGet(() -> item.getGuid().orElse("<no article>")));
@@ -39,5 +46,15 @@ public class NewsHeadlineMotdSectionGenerator extends MotdSectionGenerator {
             eb.addField(title, body, false);
         });
         return eb.build();
+    }
+
+    private record ChannelWrapper(Channel channel) {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ChannelWrapper wrapper = (ChannelWrapper) o;
+            return Objects.equals(channel.getTitle(), wrapper.channel.getTitle());
+        }
     }
 }
