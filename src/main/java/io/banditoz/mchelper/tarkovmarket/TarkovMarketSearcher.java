2578bebcdcc4b7ba5b47a13e3822da26a861cf28
HeyBanditoz <banditoz@protonmail.com>
2024-08-25 11:35:04 -0600

    Use proper singleton cache strategy for Tarkov item caching

--- src/main/java/io/banditoz/mchelper/tarkovmarket/TarkovMarketSearcher.java
+++ src/main/java/io/banditoz/mchelper/tarkovmarket/TarkovMarketSearcher.java
@@ -1,76 +1,39 @@
 package io.banditoz.mchelper.tarkovmarket;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Suppliers;
 import io.banditoz.mchelper.MCHelper;
-import io.banditoz.mchelper.utils.HttpResponseException;
 import me.xdrop.fuzzywuzzy.FuzzySearch;
 import me.xdrop.fuzzywuzzy.model.BoundExtractedResult;
 
-import java.io.IOException;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 public class TarkovMarketSearcher {
     private final MCHelper MCHELPER;
-    private final List<Item> CACHE = new ArrayList<>(2500);
-    private Instant refreshAgain;
+    private final Supplier<List<Item>> CACHE = Suppliers.memoizeWithExpiration(this::findAllItems, 15, TimeUnit.MINUTES);
 
     public TarkovMarketSearcher(MCHelper mcHelper) {
         this.MCHELPER = mcHelper;
     }
 
-    public List<Item> getMarketResultsBySearch(final String search) throws IOException, HttpResponseException {
-        if (refreshAgain == null || Instant.now().isAfter(refreshAgain)) {
-            ObjectMapper om = MCHELPER.getObjectMapper();
-            String query = """
-                    {
-                      items {
-                        name
-                        sellFor {
-                          vendor {
-                            name
-                          }
-                          priceRUB
-                        }
-                        avg24hPrice
-                        high24hPrice
-                        low24hPrice
-                        lastLowPrice
-                        wikiLink
-                        link
-                        updated
-                        imageLink
-                        changeLast48hPercent
-                        types
-                        shortName
-                      }
-                    }""";
-            Response data = MCHELPER.getHttp().getTarkovClient().getTarkovMarketData(Map.of("query", query));
-            // filter out flea market from all results
-            for (Item item : data.data().items()) {
-                item.vendorPrices().removeIf(vendorPrice -> vendorPrice.vendor().vendorName() == VendorName.FLEA);
-            }
-            CACHE.clear();
-            CACHE.addAll(data.data().items());
-            // phew, adding items succeeded! now update the refresh time
-            refreshAgain = Instant.now().plus(15, ChronoUnit.MINUTES);
-        }
+    public List<Item> getMarketResultsBySearch(final String search) {
         // so it would have been better to have a Map of Item#shortName, Item, but we can't do that because
         // shortName is not guaranteed to be unique. when I ran it, there were 11 hits of GEN3, so we have to do these
         // wonderful ~2450 string comparisons :)
+        List<Item> cachedItems = CACHE.get();
         List<Item> setAside = new ArrayList<>(15);
-        for (Item item : CACHE) {
+        for (Item item : cachedItems) {
             if (search.equalsIgnoreCase(item.shortName())) {
                 setAside.add(item);
             }
         }
         List<BoundExtractedResult<Item>> items = FuzzySearch.extractSorted(
                 search,
-                CACHE,
+                cachedItems,
                 Item::name,
                 FuzzySearch::tokenSortPartialRatio,
                 60
@@ -93,5 +56,37 @@ public class TarkovMarketSearcher {
             sortedItems.addFirst(item);
         }
         return sortedItems;
+    }
+
+    private List<Item> findAllItems() {
+        String query = """
+                {
+                  items {
+                    name
+                    sellFor {
+                      vendor {
+                        name
+                      }
+                      priceRUB
+                    }
+                    avg24hPrice
+                    high24hPrice
+                    low24hPrice
+                    lastLowPrice
+                    wikiLink
+                    link
+                    updated
+                    imageLink
+                    changeLast48hPercent
+                    types
+                    shortName
+                  }
+                }""";
+        Response data = MCHELPER.getHttp().getTarkovClient().getTarkovMarketData(Map.of("query", query));
+        // filter out flea market from all results
+        for (Item item : data.data().items()) {
+            item.vendorPrices().removeIf(vendorPrice -> vendorPrice.vendor().vendorName() == VendorName.FLEA);
+        }
+        return data.data().items();
     }
 }
